a452c4a393908bab21eb79f01c1d9c94345850e5
Archit Rohal <rohalarchit@gmail.com>
2024-11-21 22:04:53 +0530

    modify constants.js for google api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 
 export const GOOGLE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
+// export const GOOGLE_API_KEY = "AIzaSyCERZDyfPzmGEjhw2XtAYoTxzgYBw4zfy4"
 
 export const YOUTUBE_VIDEOS_API =
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=17&key=" +
